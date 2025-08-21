2b51cfe624c17c5d1a964a5e4ee88395c51991bc
franc.llerena <franc@bisuala.com>
2025-08-21 18:30:54 +0200

    UIManagerView clean code

--- TripledotFrancTATest/Assets/Tripledot/Common/Scripts/UIManagerView.cs
+++ TripledotFrancTATest/Assets/Tripledot/Common/Scripts/UIManagerView.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class UIManagerView : MonoBehaviour
 {
+    bool navBarVisible = true;
 
     //I should create a UIObjectView custom editor to hide this serialized fields from the UIObject classes but I preferred to use my time on other stuff of the test.
     [SerializeField] Transform screenTransform;
     [SerializeField] Transform windowTransform;
-
-    //NavBarManager
-    bool navBarVisible = true;
     [SerializeField] Animator navBarAnimator;
+
+
 
     private void Awake()
     {
